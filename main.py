d1a6ea18f68a0919b00db33dbee68ad52b61c4a6
light23-i <74209656+light23-i@users.noreply.github.com>
2023-12-22 15:09:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
+import serial
+import cv2
 import torch
-import cv2 
-import os 
+usbport = '/dev/tty.usbmodem1411' #usb from arduino
 
-#img = cv2.imread('pp4.jpeg')
 
-cap = cv2.VideoCapture('x.mp4')
-model = torch.load('f.pt')
-model.predict('images/M.jpeg',confidence=70).save('x.jpeg')
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == True:
-       print(model.predict(frame,confidence=70).json())
-       cv2.imshow('Frame',frame) 
+arduino = serial.Serial(usbport, 9600, timeout=0)
+
+vid = cv2.VideoCapture(0)
+distances = []
+def imgans():
+    ret,img = vid.read()
+    if ret:
+        cv2.imwrite('x1.png',img)
+        model = torch.load('f.pt')
+        return len(model.predict('x1.png',confidence=70).predictions)
     else:
-   	    break
- 
-# # When everything done, release the video capture object
-cap.release()
- 
-# # Closes all the frames
-cv2.destroyAllWindows()
+        return 0
+while (True):
+    if (arduino.inWaiting()>0):
+        myData = arduino.readline().decode('ascii')
+        if myData<10:
+            command = imgans()
+            if command==0:
+                arduino.write(180)
+            else:
+                arduino.write(-1)
+            #arduino.write(command)                          # write position to serial port
+            #reachedPos = str(arduino.readline())            # read serial port for arduino echo
+            #distances.append(myData)
+            print(myData)
+        #print(distances)
